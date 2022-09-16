d1a0fa7ad3dca7cf34e439db322bbe7810533a89
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2022-09-16 17:23:11 -0300

    settings navbar-dropdown

--- js/components/index.js
+++ js/components/index.js
@@ -8,8 +8,8 @@ function myFunction() {
   window.onclick = function(e) {
     if (!e.target.matches('.navbar-dropdown')) {
     var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+      if (myDropdown.classList.contains("show")) {
+        myDropdown.classList.remove("show");
       }
     }
   }
