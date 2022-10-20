5e4dbf2096024ef2dde49f9c6c3ac1c6fe7559ed
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2022-10-20 17:32:47 -0300

    add icon close in menu toggle

--- js/components/index.js
+++ js/components/index.js
@@ -4,6 +4,10 @@ const menuSection = document.querySelector(".navbar-container")
 const menuToggle = document.querySelector(".menu-toggle")
 const backgroundTransparent = document.querySelector(".nav-background-transparent")
 
+const toggleOne = document.querySelector(".one")
+const toggleTwo = document.querySelector(".two")
+const toggleThree = document.querySelector(".three")
+
 const toggleFeatures = document.getElementById("dropdownFeatures")
 const toggleCompany = document.getElementById("dropdownCompany")
 const dropdownFeat = document.getElementById("dropdownFeat")
@@ -12,8 +16,12 @@ const arrowFeat = document.querySelector(".navbar-dropdown-icon")
 const arrowComp = document.querySelector(".navbar-dropdown-icon.-company")
 
 
+
 menuToggle.addEventListener("click", () => {
     backgroundTransparent.style.display = show ? "block" : "none"
+    toggleOne.style.transform = show ? "matrix(0.71,0.71,-0.71,0.71,7,7)" : "initial"
+    toggleTwo.style.opacity = show ? "0" : "initial"
+    toggleThree.style.transform = show ? "matrix(0.71,-0.71,0.71,0.71,6,-10)" : "initial"
     menuSection.classList.toggle("on", show)
     show = !show;
 })
